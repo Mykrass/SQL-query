64b746de7a266056ca53eaa2c50718cf426ff208
Igor Krasovskiy <mykras@me.com>
2022-09-14 16:26:20 +0200

    Update change_date_type_table_users_script.sql

--- change_date_type_table_users_script.sql
+++ change_date_type_table_users_script.sql
@@ -1,2 +1,4 @@
 ALTER TABLE users ALTER COLUMN created TYPE INTEGER USING created::integer;
 ALTER TABLE users ALTER COLUMN created TYPE DATE USING to_timestamp(created);
+-- or --
+ALTER TABLE users ALTER COLUMN created TYPE TIMESTAMP WITH TIME ZONE USING created::timestamptz;
