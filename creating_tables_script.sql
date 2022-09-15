1012951a779b215b983b770aa6bbd42dd93bd786
Igor Krasovskiy <mykras@me.com>
2022-09-15 19:09:19 +0200

    Update creating_tables_script.sql
    Creating table Payments.

--- creating_tables_script.sql
+++ creating_tables_script.sql
@@ -31,7 +31,6 @@ DELIMITER ';' CSV HEADER;
 
 ALTER TABLE AdImpression ALTER COLUMN date TYPE DATE USING to_timestamp(date);
 ALTER TABLE AdImpression ALTER COLUMN install_date TYPE DATE USING to_timestamp(install_date);
---ALTER TABLE AdImpression ALTER COLUMN install_date TYPE TIMESTAMP WITH TIME ZONE USING install_date::timestamptz;
 
 
 
@@ -65,5 +64,38 @@ ALTER TABLE GameSessions ALTER COLUMN install_date TYPE DATE USING to_timestamp(
 
 
 
+drop table Payments;
+create table Payments (
+devtodev_id int,
+date int,
+level int,
+transaction_id text,
+transaction_name text,
+amount_in_usd float,
+status int,
+install_date int,
+isTester bool,
+cheat bool,
+app_version text
+);
 
+\COPY Payments(
+devtodev_id,
+date,
+level,
+transaction_id,
+transaction_name,
+amount_in_usd,
+status,
+install_date,
+isTester,
+cheat,
+app_version
+  ) FROM '/Users/mykras/Downloads/StepicoDB_Summer/csv_to_db/Payments.csv'
+DELIMITER ';' CSV HEADER;
+  
+ALTER TABLE Payments ALTER COLUMN date TYPE DATE USING to_timestamp(date);
+ALTER TABLE Payments ALTER COLUMN install_date TYPE DATE USING to_timestamp(install_date);
+
+  
   
