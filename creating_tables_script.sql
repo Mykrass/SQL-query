b8416ffb4886182f4bf52d8ccc9533f3363db6af
Igor Krasovskiy <mykras@me.com>
2022-09-15 17:53:17 +0200

    Create creating_tables_script.sql
    table AdImpression

---
+++ creating_tables_script.sql
@@ -0,0 +1,33 @@
+drop table AdImpression;
+create table AdImpression (
+devtodev_id int,
+date text,
+level int,
+placement text,
+network text,
+unit text,
+source text, 
+revenue float,
+install_date text,
+isTester bool,
+cheat bool,
+app_version text);
+
+\COPY AdImpression(
+devtodev_id,
+date,
+level,
+placement,
+network,
+unit,
+source, 
+revenue,
+install_date,
+isTester,
+cheat,
+app_version
+  ) FROM '/Users/mykras/Downloads/StepicoDB_Summer/csv_to_db/AdImpression.csv '
+DELIMITER ';' CSV HEADER;
+
+
+
