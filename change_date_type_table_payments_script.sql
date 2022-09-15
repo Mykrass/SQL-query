74db250c9c364cebeac9362b5c97870c556fabe4
Igor Krasovskiy <mykras@me.com>
2022-09-15 19:29:13 +0200

    Delete change_date_type_table_payments_script.sql

--- change_date_type_table_payments_script.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE payments ALTER COLUMN date TYPE INTEGER USING date::integer;
-ALTER TABLE payments ALTER COLUMN date TYPE DATE USING to_timestamp(date);
-
-ALTER TABLE payments ALTER COLUMN created TYPE INTEGER USING created::integer;
-ALTER TABLE payments ALTER COLUMN created TYPE TIMESTAMP WITH TIME ZONE USING created::timestamptz;
