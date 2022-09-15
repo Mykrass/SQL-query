a3c80ec2b7aa4fc006f9f62e24253bdf97c24b5c
Igor Krasovskiy <mykras@me.com>
2022-09-15 19:29:02 +0200

    Delete change_date_type_table_users_script.sql

--- change_date_type_table_users_script.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE users ALTER COLUMN created TYPE INTEGER USING created::integer;
-ALTER TABLE users ALTER COLUMN created TYPE DATE USING to_timestamp(created);
--- or --
-ALTER TABLE users ALTER COLUMN created TYPE TIMESTAMP WITH TIME ZONE USING created::timestamptz;
