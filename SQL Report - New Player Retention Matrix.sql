581cd50465bf7dec364e9b11e0ae036098a20c4d
Igor Krasovskiy <mykras@me.com>
2022-11-08 18:06:14 +0100

    Update SQL Report - New Player Retention Matrix.sql

--- SQL Report - New Player Retention Matrix.sql
+++ SQL Report - New Player Retention Matrix.sql
@@ -7,12 +7,23 @@ https://github.com/deltaDNA/sql-cookbook/blob/master/KPIs/New_Player_Retention_M
 -- New Player Retention Matrix
 
 
+/*
+/*
+https://github.com/deltaDNA/sql-cookbook/blob/master/KPIs/New_Player_Retention_Matrix.sql
+*/
+
+----------------------------
+-- New Player Retention Matrix
+
+declare 
+@install_interval interval = '7 day';
+
 with days_data as (
 select devtodevid 
 		, created::date as created
 		, eventtime::date as eventtime
 	from p106255.sessions
-	--where created >= CURRENT_DATE - 30
+	where created > current_date - @install_interval and eventtime < current_date
 	group by 1, 2, 3
 ),
 
@@ -33,7 +44,21 @@ select created
 	, count (distinct case when eventtime - created = 12 then devtodevid else null end) as "R12"
 	, count (distinct case when eventtime - created = 13 then devtodevid else null end) as "R13"
 	, count (distinct case when eventtime - created = 14 then devtodevid else null end) as "R14"
+	, count (distinct case when eventtime - created = 15 then devtodevid else null end) as "R15"
+	, count (distinct case when eventtime - created = 16 then devtodevid else null end) as "R16"
+	, count (distinct case when eventtime - created = 17 then devtodevid else null end) as "R17"
+	, count (distinct case when eventtime - created = 18 then devtodevid else null end) as "R18"
+	, count (distinct case when eventtime - created = 19 then devtodevid else null end) as "R19"
+	, count (distinct case when eventtime - created = 20 then devtodevid else null end) as "R20"
 	, count (distinct case when eventtime - created = 21 then devtodevid else null end) as "R21"
+	, count (distinct case when eventtime - created = 22 then devtodevid else null end) as "R22"
+	, count (distinct case when eventtime - created = 23 then devtodevid else null end) as "R23"
+	, count (distinct case when eventtime - created = 24 then devtodevid else null end) as "R24"
+	, count (distinct case when eventtime - created = 25 then devtodevid else null end) as "R25"
+	, count (distinct case when eventtime - created = 26 then devtodevid else null end) as "R26"
+	, count (distinct case when eventtime - created = 27 then devtodevid else null end) as "R27"
+	, count (distinct case when eventtime - created = 28 then devtodevid else null end) as "R28"
+	, count (distinct case when eventtime - created = 29 then devtodevid else null end) as "R29"
 	, count (distinct case when eventtime - created = 30 then devtodevid else null end) as "R30"
 from days_data
 group by 1
